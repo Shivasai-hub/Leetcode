09bbe915d52165bdf551903989258c2e7224583d
Shivasai Gajengi <gajengishivasai@gmail.com>
2026-03-13 20:11:17 +0530

    Time: 5 ms (79.49%), Space: 45.9 MB (70.25%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+        {
+            return false;
+        }
+        int rem=0;
+        int rev=0;
+        int temp=x;
+        while(x!=0)
+        {
+            rem=x%10;
+            rev=rev*10+rem;
+            x=x/10;
+        }
+        if(rev==temp)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
