84caeb9373ae9c5761c6087ba81bc32d97f6e5e4
Shivasai Gajengi <ugs24037_cse.gajengi@cbit.org.in>
2026-03-30 22:30:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -1,32 +1,28 @@
 class Solution {
     public boolean checkStrings(String s1, String s2) {
-        char[] a1 = s1.toCharArray();
-        char[] a2 = s2.toCharArray();
-        HashMap<Character,Integer> e1 = new HashMap<>();
-        for(int i = 0;i<s1.length();i+=2)
-        {
-            e1.put(a1[i],e1.getOrDefault(a1[i],0)+1);
+        int  n = s1.length();
+        int freq1[]=new int[26];
+        for(int i=0;i<n;i+=2){
+            freq1[s1.charAt(i)-'a']++;
         }
-        HashMap<Character,Integer> e2 = new HashMap<>();
-        for(int i=0;i<s2.length();i+=2)
-        {
-            e2.put(a2[i],e2.getOrDefault(a2[i],0)+1);
+        int freq2[]=new int[26];
+        for(int i=0;i<n;i+=2){
+            freq2[s2.charAt(i)-'a']++;
         }
-        boolean ans1 = e1.equals(e2);
-        if(ans1 == false)
-        {
-            return false;
+        for(int i =0;i<26;i++){
+            if(freq1[i]!=freq2[i])return false;
+            freq1[i]=0;
+            freq2[i]=0;
         }
-        HashMap<Character,Integer> o1 = new HashMap<>();
-        for(int i=1;i<s1.length();i+=2)
-        {
-            o1.put(a1[i],o1.getOrDefault(a1[i],0)+1);
+        for(int i=1;i<n;i+=2){
+            freq1[s1.charAt(i)-'a']++;
         }
-        HashMap<Character,Integer> o2 = new HashMap<>();
-        for(int i=1;i<s2.length();i+=2)
-        {
-            o2.put(a2[i],o2.getOrDefault(a2[i],0)+1);
+        for(int i=1;i<n;i+=2){
+            freq2[s2.charAt(i)-'a']++;
         }
-        return o1.equals(o2);
+        for(int i =0;i<26;i++){
+            if(freq1[i]!=freq2[i])return false;
+        }
+        return true;
     }
 }
