87b8b58e262a91fa1c60e7a46754a5d6b120e10d
Shivasai Gajengi <ugs24037_cse.gajengi@cbit.org.in>
2026-04-28 19:29:40 +0530

    Time: 0 ms (100%), Space: 42 MB (87.27%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int ans = 0;
+        while(n != 1)
+        {
+            if(n % 2 == 0)
+            {
+                n = n / 2;
+                ans = ans + n;
+            }
+            else
+            {
+                n = (n - 1)/2 + 1;
+                ans = ans + n - 1;
+            }
+        }
+        return ans;
+    }
+}
